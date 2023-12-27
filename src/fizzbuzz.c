2d3de894ad930091bc4a76709f73399aba21cda3
Fader233 <101299991+Fader233@users.noreply.github.com>
2023-12-27 11:28:54 +0800

    Update fizzbuzz.c

--- src/fizzbuzz.c
+++ src/fizzbuzz.c
@@ -1,3 +1,26 @@
 #include <stdio.h>
 #include "fizzbuzz.h"
 
+int AddWord (char res[100], char target[100], int ptr)
+{
+    int i = 0;
+    while (target[i] != '\0') {
+        res[ptr] = target[i];
+        i += 1;
+        ptr += 1;
+    }
+    return ptr;
+}
+
+void CallNumGame(int num, Match *matchs, char res[100])
+{
+    int ptr = 0;
+    for(int i = 0; i < matchs->length; i ++){
+        if (num % matchs->items[i].number == 0) {
+            ptr = AddWord (res, matchs->items[i].word, ptr);
+        }
+    }
+    res[ptr] = '\0';
+    return;
+}
+
