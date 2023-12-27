4817787c282d023d9366ef1fbc2d6327285bc59d
Fader233 <101299991+Fader233@users.noreply.github.com>
2023-12-27 10:28:38 +0800

    ADD initial files

---
+++ include/fizzbuzz.h
@@ -0,0 +1,13 @@
+#ifndef __FIZZBUZZ_H__
+#define __FIZZBUZZ_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __FIZZBUZZ_H__ */
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "fizzbuzz.h"
+
+int main()
+{
+    return 0;
+}
