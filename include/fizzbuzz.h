10c2dd7ef430de8c22387c2b1bf2a41fd19c4017
Fader233 <101299991+Fader233@users.noreply.github.com>
2023-12-27 11:29:22 +0800

    Update fizzbuzz.h

--- include/fizzbuzz.h
+++ include/fizzbuzz.h
@@ -5,6 +5,18 @@
 extern "C" {
 #endif
 
+typedef struct _Matching_Item {
+    char word[30];
+    int number;
+}Matching_Item;
+
+typedef struct Matching_Items
+{
+    int length;
+    Matching_Item items[100];
+}Match;
+
+void CallNumGame(int num, Match *matchs, char res[100]);
 
 #ifdef __cplusplus
 }
